f6dda529a848acc1f819d6245eff9621159bb9ea
Barnabas <shaibuanibe@gmail.com>
2023-11-20 12:44:09 +0100

    changes name to Barnabas Shaibu

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -9,7 +9,7 @@ const Hero = () => (
     <LeftSection>
       <SectionTitle main center>
         Hi There, I Am <br />
-        Shaibu Barnabas 
+        Barnabas Shaibu 
       </SectionTitle>
       <SectionText>
       I'm an experienced React frontend developer specializing in user-centric, high-performance web applications. With expertise in responsive design and strong collaboration skills, I deliver outstanding digital experiences. My portfolio showcases successful projects, and I'm committed to continuous learning and innovation. Let's collaborate on your next project for exceptional results!
