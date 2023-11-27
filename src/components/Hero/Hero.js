97bf8bf75fc8d8af4826c100c5e5ad0e909bd923
Barnabas <shaibuanibe@gmail.com>
2023-11-27 10:39:05 +0100

    corrected learn more

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -15,7 +15,7 @@ const Hero = () => (
       I'm an experienced frontend software engineer specializing in user-centric, high-performance web applications. With expertise in responsive design and strong collaboration skills, I deliver outstanding digital experiences. My portfolio showcases successful projects, and I'm committed to continuous learning and innovation. Let's collaborate on your next project for exceptional results!
 
       </SectionText>
-      <Button onClick={() => window.location = 'https://drive.google.com/file/d/1b6NKWFwt4eFmxTGeCigGkd9rDhx5luqR/view?usp=sharing'}>
+      <Button onClick={() => window.location = 'https://drive.google.com/file/d/1mwNOMxaTPnq5PyJ3RHCgZ8XuwkL8zAmI/view?usp=sharing'}>
         Learn More
       </Button>
     </LeftSection>
